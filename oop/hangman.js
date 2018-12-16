501e7177c25e0e6476c0420714d2b9e51447d941
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-12-16 11:35:18 +0200

    some func in hangman

--- oop/hangman.js
+++ oop/hangman.js
@@ -9,36 +9,55 @@ const Hangman = function(word, remainingGuesses){
 Hangman.prototype.getPuzzle = function(){
     let array = [this.word];
     // console.log(array[0][1])
-    // console.log('-> ' + array[0].length)
-
     return array
 }
 
 
 const game = new Hangman('Cat', 3)
+const guess = new Hangman('Cat')
 
-const guess = new Hangman('Catt')
 let baseWord = []
 
-if (game.word === guess.word){
-    console.log('arvasit oikein')
-} else {
-    let i = 0
+let yourGuesses = function() {
+    if (game.word === guess.word){
+        console.log('arvasit oikein')
+    // } else if (game.word !== guess.word){
+    //     let i = 0
+    //     while (i < guess.length) {
+    //         baseWord.push(guess.word[i])
+    //         i++;
+    //         }
 
-    while (i < game.wordLen) {
-        baseWord.push('*')
-        i++;
-      }
-
-
+    } else {
+        let i = 0
+        while (i < game.wordLen) {
+            baseWord.push('*')
+            i++;
+            }
+    }    
 }
-console.log('-> ' + game.getPuzzle())
-console.log(baseWord)
 
-let i = 0
-while (i < game.wordLen) {
-    console.log(baseWord[i])
-    process.stdout.write(baseWord[i]);
+yourGuesses(guess)
+
+// let howManyGuesses = function() {
+    process.stdout.write(`No guesses? -> ` );
+    let i = 0
+    while (i <= game.wordLen - 1) {
+        process.stdout.write(`*` );
+        i++;
+        }
+     
+     console.log()
+// }
+
+// howManyGuesses()
+
+i = 0
+process.stdout.write(`Guessed `)
+
+while (i < game.wordLen-1) {
+    process.stdout.write(`"${baseWord[i]}",` );
     i++;
   }
-
+  process.stdout.write(` and "${baseWord[i]}"?`)
+  process.stdout.write(` -> ${game.getPuzzle()} `)
