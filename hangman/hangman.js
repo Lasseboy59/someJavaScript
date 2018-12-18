303dc886c7db6664bb478b8a4277342a370daa97
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-12-18 13:58:55 +0200

    my solution off DomManipulation

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -33,36 +33,3 @@ Hangman.prototype.addGuess = function(guess) {
         this.remainingGuesses--
     }
 }
-
-
-const game1 = new Hangman('Cat', 2)
-// game1.guessedLetters = 'c*t'
-
-// console.log(game1.getPuzzle())
-// let puzzle = game1.getPuzzle()
-// console.log(game1.remainingGuesses)
-// let puzzle2 = game1.remainingGuesses
-
-window.addEventListener('keypress', function (e) {
-    const guess = String.fromCharCode(e.charCode)
-    game1.addGuess(guess)
-    console.log(game1.getPuzzle())
-    console.log(game1.remainingGuesses)
-    let puzzle = game1.getPuzzle()
-    let puzzle2 = game1.remainingGuesses
-    showOnPage(puzzle, puzzle2)
-})
-
-
-// Generate the DOM structure for a note
-    const showOnPage = function(puzzle, puzzle2) {
-        document.querySelector('#notes').innerHTML = ''
-        const element = document.getElementById('notes')
-        var linebreak = document.createElement('br');
-
-        var textNode = document.createTextNode('The word: ' + puzzle);
-        element.appendChild(textNode);
-        element.appendChild(linebreak);
-        textNode = document.createTextNode('Number of guesses ' + puzzle2);
-        element.appendChild(textNode);
-    }
