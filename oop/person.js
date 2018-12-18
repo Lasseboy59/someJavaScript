beb2b4cae00fe435dbb5a77a380988ef1dd61152
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-12-18 19:06:56 +0200

    changed from constructor function presentation to class syntax - with Hangman

--- oop/person.js
+++ oop/person.js
@@ -1,33 +1,64 @@
 // Prototype Inheritance
 
-const Person = function(firstName, lastName, age, likes = []){
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
-}
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
 
-Person.prototype.getBio = function(){
-    let bio = `${this.firstName} is ${this.age}.`
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
 
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+    
+        return bio
+    }
 
-    return bio
-}
-
-Person.prototype.setName = function(fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
 const me = new Person('Lasse', 'Ojala', 45, ['Teachin', 'Biking'])
-
 me.setName('Aleksis Turner')
 console.log(me.getBio())
 
 const two = new Person('Tim', 'Turner', 39)
-
 console.log(two.getBio())
+
+
+
+// const myPerson = new PersonClass('Lasse', 'Ojala', 45, ['Reading'])
+// console.log(myPerson.getBio())
+
+// const Person = function(firstName, lastName, age, likes = []){
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.age = age
+//     this.likes = likes
+// }
+
+// Person.prototype.getBio = function(){
+//     let bio = `${this.firstName} is ${this.age}.`
+
+//     this.likes.forEach((like) => {
+//         bio += ` ${this.firstName} likes ${like}.`
+//     })
+
+//     return bio
+// }
+
+// Person.prototype.setName = function(fullName) {
+//     const names = fullName.split(' ')
+//     this.firstName = names[0]
+//     this.lastName = names[1]
+// }
+
+
+
