819db3e570cf3a502b4c07f23bee9ef96f5750a3
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-12-20 16:29:54 +0200

    converting to async

--- hangman/requests.js
+++ hangman/requests.js
@@ -1,4 +1,15 @@
-const getPuzzle = (wordCount) => {
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    
+    if(response.status === 200) {
+        const data = await response.json()
+        return data.puzzle
+    } else {
+        throw new Error('Unable to get puzzle')
+    }
+}
+
+const getPuzzleOld = (wordCount) => {
     return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
         if(response.status === 200) {
             return response.json()
@@ -10,7 +21,18 @@ const getPuzzle = (wordCount) => {
     })
 }
 
-const getCountryDetails = (countryCode) => {
+const getCountryDetails = async (countryCode) => {
+    const response = await fetch(`http://restcountries.eu/rest/v2/all`)
+
+    if(response.status === 200) {
+        const data = await response.json()
+        return data.find((country)=>country.alpha2Code===countryCode)
+    } else{
+        throw new Error ('Unable to get data')
+    }
+}
+
+const getCountryDetailsOld = (countryCode) => {
     return fetch(`http://restcountries.eu/rest/v2/all`).then((response) => {
         if(response.status === 200) {
             return response.json()
@@ -22,13 +44,23 @@ const getCountryDetails = (countryCode) => {
     })
 }
 
+const getLocation = async () => {
+    const response = await fetch(`https://ipinfo.io/json?token=3d4e7ffcf7483a`)
 
-const getLocation = () => {
+    if(response.status === 200){
+        const location = await response.json()
+        return location
+    } else{
+        throw new Error('Unable to get location')
+    }
+}
+
+const getLocationOld = () => {
     return fetch(`https://ipinfo.io/json?token=3d4e7ffcf7483a`).then((response) => {
         if(response.status === 200) {
             return response.json()
         } else{
-            throw new Error('Unable to fetch puzzle')
+            throw new Error('Unable to fetch location')
         }
     })
 }
