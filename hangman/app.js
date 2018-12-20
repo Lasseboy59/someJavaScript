ffd726c315d3df7a20e190297bdad5074f2a3f52
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-12-20 13:17:19 +0200

    made some  - cleaning code

--- hangman/app.js
+++ hangman/app.js
@@ -26,12 +26,6 @@ window.addEventListener('keypress', (e) => {
     statusEl.textContent =  game1.GameStatus
 })
 
-// getPuzzle('2').then((data) => {
-//     console.log(data.puzzle)
-// }, (err) => {
-//     console.log(`Error: ${err}`)
-// })
-
 getPuzzle('2').then((puzzle) => {
     console.log(puzzle)
 }).catch((err) => {
@@ -45,23 +39,3 @@ getCountryDetails('FI').then((country) => {
 console.log(`Error: ${err}`)
 })
 
-
-// getCountryDetails('FI').then((country) => {
-//     console.log(`Country name: ${country.name}, population: ${country.population}`)
-// }, (err) => {
-//     console.log(`Error: ${err}`)
-// })
-
-
-
-// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
-//     if(response.status === 200) {
-//         return response.json()
-//     } else {
-//         throw new Error('Unable to fetch the puzzle')
-//     }
-// }).then((data) => {
-//     console.log(data.puzzle)
-// }).catch((err) => {
-//     console.log(err)
-// })
--- hangman/requests.js
+++ hangman/requests.js
@@ -21,34 +21,3 @@ const getCountryDetails = (countryCode) => {
         return data.find((country)=>country.alpha2Code===countryCode)
     })
 }
-
-
-// const getCountryDetails = (countryCode) => new Promise((resolve, reject) => {
-//     const request = new XMLHttpRequest()
-//     const countryRequest = new XMLHttpRequest()
-//     this.countryCode = countryCode
-    
-//     countryRequest.addEventListener('readystatechange', (e) => {
-//         if(e.target.readyState === 4 && e.target.status === 200) {
-//             const data = JSON.parse(e.target.responseText)
-//             let countries = e.target.responseText
-//             let code = 'alpha2code'
-     
-//             const country = data.find((country) => {
-//                 if (country.alpha2Code === countryCode) {
-//                     return true
-//                 } else {
-//                     return false
-//                 }
-//             })
-//             resolve(country)
-    
-            
-//         } else if(e.target.readyState === 4) {
-//             reject('Unable to fetch data')
-//         }
-//     })
-    
-//     countryRequest.open('GET','http://restcountries.eu/rest/v2/all')
-//     countryRequest.send()
-// })
