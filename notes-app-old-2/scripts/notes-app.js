c3738104c9b229fc3ec31a0d47e7864d6945f112
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-12-28 17:50:53 +0200

    added lines to notes-app-old-2 gained some functionality

--- notes-app-old-2/scripts/notes-app.js
+++ notes-app-old-2/scripts/notes-app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+
 let notes = getSavedNotes()
 
 const filters = {
@@ -10,25 +11,47 @@ const filters = {
 renderNotes(notes, filters)
 
 
-document.querySelector('#create-note').addEventListener('click', (e) => {
-    const id = uuidv4()
-    const timestamp = moment().valueOf()
-    notes.push({
-         id: id,
-         title: '',
-         body: '',
-         recipe: {
-             ingredients: '',
-             available: false
-         },
-         createdAt: timestamp,
-         updatedAt: timestamp,
+// document.querySelector('#create-note').addEventListener('click', (e) => {
+//     const idVar = uuidv4()
+//     const timestamp = moment().valueOf()
+//     notes.push({
+//          id: idVar,
+//          createdAt: timestamp,
+//          updatedAt: timestamp,
+//          title: '',
+//          body: ''
+//     })
+//     saveNotes(notes)
+//     location.assign(`edit.html#${idVar}`)
+// })
 
-    })
-    saveNotes(notes)
-    location.assign(`edit.html#${idVar}`)
+document.querySelector('#create-note').addEventListener('click', (e) => {
+
+    console.log('--- addNote --')
+    addNote()
+
+
 })
 
+// document.querySelector('#create-note').addEventListener('click', (e) => {
+
+//         const addNote = () => {
+
+//             const newNote = {
+//                 timestamp = moment().valueOf(),
+//                 id: uuidv4(),
+//                 title: '',
+//                 body: '',
+//                 ingredients: [],
+//                 createdAt: timestamp,
+//                 updatedAt: timestamp,
+//                 };
+
+//             notes.push(newNote);
+//             saveNotes(notes)
+//             location.assign(`edit.html#${idVar}`)
+//     }
+// })
 
 document.querySelector('#search-text').addEventListener('input', (e) => {
     filters.searchText = e.target.value
@@ -43,3 +66,23 @@ window.addEventListener('storage', (e) =>{
     }
 })
 
+let id =  uuidv4()
+
+const addNote = () => {
+
+    const newNote = {
+        // const timestamp = moment().valueOf(),
+        id: id,
+        title: '',
+        body: '',
+        ingredients: [],
+        // createdAt: timestamp,
+        // updatedAt: timestamp,
+        };
+
+    notes.push(newNote);
+    saveNotes(notes)
+    location.assign(`edit.html#${id}`)
+}
+
+// export { addNote }
