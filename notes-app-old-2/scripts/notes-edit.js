9ecc3d0c6d4afb8ee7791aeb253a1115677a86e8
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-12-28 10:17:12 +0200

    starting with notes-app-old-2 (recipe)

---
+++ notes-app-old-2/scripts/notes-edit.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const recipeElement = document.querySelector('#note-recipe')
+
+const removeElement = document.querySelector('#remove-note')
+const lastEdited = document.querySelector('#last-edited')
+
+const noteId = location.hash.substring(1)
+let notes = getSavedNotes()
+let note = notes.find((note) => note.id === noteId)
+
+if(!note) {
+    location.assign('/index.html')
+}
+
+titleElement.value = note.title
+bodyElement.value = note.body
+recipeElement.value = note.recipe
+
+lastEdited.textContent = generateLastEdited(note.updatedAt)
+
+titleElement.addEventListener('input', (e) => {
+    note.title = e.target.value
+    note.updatedAt = moment().valueOf()
+    lastEdited.textContent = generateLastEdited(note.updatedAt)
+    saveNotes(notes)
+})
+
+bodyElement.addEventListener('input', (e) => {
+    note.body = e.target.value
+    note.updatedAt = moment().valueOf()
+    lastEdited.textContent = generateLastEdited(note.updatedAt)
+    saveNotes(notes)
+})
+
+recipeElement.addEventListener('input', (e) => {
+    note.recipe = e.target.value
+    note.updatedAt = moment().valueOf()
+    lastEdited.textContent = generateLastEdited(note.updatedAt)
+    saveNotes(notes)
+})
+
+
+removeElement.addEventListener('click', (e) => {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/index.html')
+})
+
+removeElement.addEventListener('click', (e) => {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/index.html')
+})
+
+window.addEventListener('storage', (e) => {
+    if(e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        note = notes.find((note) =>note.id === noteId)
+        
+        if(!note) {
+            location.assign('/index.html')
+        }
+        
+        titleElement.value = note.title
+        bodyElement.value = note.body
+        recipeElement.value = note.recipe
+        lastEdited.textContent = generateLastEdited(note.updatedAt)
+    }
+})
